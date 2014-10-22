6f0337fb3f2bc2a2f69fafb2d0841b68d621f5dd
wuhailin <690019177@qq.com>
2014-10-23 00:05:51 +0800

    test

--- admin/views/layouts/main.php
+++ admin/views/layouts/main.php
@@ -22,5 +22,22 @@ $this->app->clientScript->registerCssFile('/css/styles.css');
     </head>
     <body>
     <?php echo $content;?>
+    <input type="button" id="test1" value="test1"/>
+    <input type="button" id="test2" value="test"/>
+    <input type="text" id="test3"/>
     </body>
+    <script>
+        $(function(){
+            var test3 = $('#test3'),
+                test2 = $('#test2');
+            test2.toggle(function(){
+                test3.val(1);
+            },function(){
+                test3.val(2);
+            });
+            $('#test1').on('click', function(){
+                test2.click();
+            });
+        });
+    </script>
 </html>
